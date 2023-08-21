c252181caca713f5aecc3ee04216527b8ea12c92
Vinay Kumar <vinaykumary122@gmail.com>
2023-08-21 13:08:45 +0530

    Add game reset button(new game button)

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import { useState } from "react";
 import { calculateWinner } from './winner';
 import History from './components/History';
 
+const NEW_GAME = [{squares: Array(9).fill(null), isXNext: false}]
+
 function App(){
 
     // const [squares, setSquares] = useState(Array(9).fill(null));
     // const [isXNext, setIsXNext] = useState(false);
-    const [history, setHistory] = useState([{squares: Array(9).fill(null), isXNext: false}]);
+    const [history, setHistory] = useState(NEW_GAME);
     const [currMove, setCurrMove] = useState(0);
 
     const gamingBoard = history[currMove];
 
     const winner = calculateWinner(gamingBoard.squares);
 
-    console.log({history,currMove});
+    // console.log({history,currMove});
 
     const handleSquareClick = (clickedPosition) => {
 
@@ -49,6 +51,11 @@ function App(){
         setCurrMove(move);
     }
 
+    const onNewGameStart = () => {
+        setHistory(NEW_GAME);
+        setCurrMove(0);
+    }
+
     return(
         <div className='app'>
             <h1 style={{color:"grey"}}>Tic Tac Toe</h1>
@@ -56,6 +63,8 @@ function App(){
             <StatusMessage winner={winner} gamingBoard={gamingBoard}/>
 
             <Board squares={gamingBoard.squares} handleSquareClick={handleSquareClick}/>
+
+            <button type='button' onClick={onNewGameStart} className={`btn-reset ${winner ? 'active' : ''}`}>New Game</button>
 
             <h2>Game Hostory.🎰</h2>
             <History history={history} moveTo={moveTo} currMove={currMove}/>
